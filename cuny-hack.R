ac5ba2e0dbf0f3dfcd73bfe0f9d75006fcfa42ae
vikivayner <63611640+vikivayner@users.noreply.github.com>
2021-01-23 23:10:28 -0500

    fixed violation.county, checked if i have and null

--- cuny-hack.R
+++ cuny-hack.R
@@ -17,3 +17,27 @@ count_state <- Aug_2013_June_2014 %>% count(Registration.State)
 new_Aug2013June2014 <- Aug_2013_June_2014[-c(10:12,17:19,37:50)]
 new_Aug2013June2014 <- new_Aug2013June2014[-c(31)]
 
+#checking for na
+
+colSums(is.na(new_Aug2013June2014))
+
+
+
+
+#fixed names in violation.county
+fix_vio_Aug2013Aug2014 <- new_Aug2013June2014 %>% mutate(Violation.County= case_when(Violation.County == "BX" ~ "BRONX",
+                                                                                     Violation.County == "K" ~ "KINGS", 
+                                                                                     Violation.County == "Q" ~ "QUEENS", 
+                                                                                     Violation.County == "QUEEN" ~ "QUEENS", 
+                                                                                     Violation.County == "RC" ~ "RICHMOND",
+                                                                                     Violation.County == "RICH" ~ "RICHMOND", 
+                                                                                     Violation.County == "R" ~ "RICHMOND",
+        
+                                                                                     
+ #looking over violation.county for unique values                                                                                                                                                                  TRUE ~ Violation.County))
+unique_county <- print(unique(fix_vio_Aug2013Aug2014[c("Violation.County")]))
+#looking at unique precincts 
+unique_precinct<- print(unique(fix_vio_Aug2013Aug2014[c("Violation.Precinct")]))
+
+
+write.csv(new_Aug2013June2014, file = "CUNY Hackathon Aug2013June2014.csv")
